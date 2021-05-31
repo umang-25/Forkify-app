699523cfb67cb4844cdb722189934872255bab3f
umang-25 <f20170510@goa.bits-pilani.ac.in>
2021-05-31 20:24:33 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '900dc62e-4852-410a-8309-ea3410732f44';
+export const MODAL_CLOSE_SEC = 2.5;
